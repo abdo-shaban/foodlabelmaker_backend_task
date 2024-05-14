821208929128a97552887902fd1d17dd0705f94d
Abdel Rahman Shaban <a.shaban@malaeb.com>
2024-05-14 21:16:20 +0300

    prepare admin account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       $checkAdminExists = User::query()->where('email', 'admin@example.com')->exists();
+        // Check if admin user already exists
+        if (! $checkAdminExists)
+        {
+            // Create admin user if not already existing
+            User::factory()->create([
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+            ]);
+        }
     }
 }
